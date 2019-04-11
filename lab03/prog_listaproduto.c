10578c37552202c0838e90176e51a076655a4dfd
Ezequiel <quielemao@gmail.com>
2019-04-10 21:56:57 -0300

    correção

--- lab03/prog_listaproduto.c
+++ lab03/prog_listaproduto.c
@@ -3,11 +3,12 @@
 int main(){
     tProduto prod;
     tLista lista;
+    int continua;
     
     do{
         criaProduto(&prod); /*Cria um produto*/
         printf("\nDESEJA INCLUIR OUTRO PRODUTO?\n(1 - PARA CONTINUAR, 0 - PARA PARAR)");
-        scanf("%c", continua);
+        scanf(" %c", continua);
     } while(continua == 1)
 
     /*Cria a lista*/
